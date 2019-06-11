5a4d1fe60850fa1624f56b8be79ebc8f10fb806b
karlv <karl.vana@proekspert.ee>
2019-06-11 16:06:59 +0300

    Domain refractor

--- src/main/java/com/example/demo/domain/Answer.java
+++ src/main/java/com/example/demo/domain/Answer.java
@@ -5,40 +5,6 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 
 import javax.persistence.*;
 
-/*
-@Entity
-@JsonInclude(JsonInclude.Include.NON_NULL)
-public class Answer {
-
-    @Id
-    @GeneratedValue
-    private Integer id;
-    private String text;
-    private Boolean isCorrect;
-
-    public Answer() {
-
-    }
-    public Answer(String text, Boolean isCorrect) {
-        this.text = text;
-        this.isCorrect = isCorrect;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    @JsonIgnore
-    public Boolean getCorrect() {
-        return isCorrect;
-    }
-
-}
- */
 @Entity
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Answer {
--- src/main/java/com/example/demo/domain/Question.java
+++ src/main/java/com/example/demo/domain/Question.java
@@ -5,39 +5,6 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import javax.persistence.*;
 import java.util.List;
 
-/*
-@Entity
-public class Question {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
-    private String questionText;
-    @OneToMany(cascade = CascadeType.ALL)
-    @JoinColumn()
-    private List<Answer> answerList;
-
-    public Question() {
-    }
-
-    public Question(String questionText, List<Answer> answerList) {
-        this.questionText = questionText;
-        this.answerList = answerList;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public String getQuestionText() {
-        return questionText;
-    }
-
-    public List<Answer> getAnswerList() {
-        return answerList;
-    }
-}
- */
 @Entity
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Question {

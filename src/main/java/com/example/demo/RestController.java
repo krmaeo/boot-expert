6386a25e65d92c753995e8f1c76a74869e2fc7db
karlv <karl.vana@proekspert.ee>
2019-06-12 16:39:56 +0300

    Added sessions

--- src/main/java/com/example/demo/RestController.java
+++ src/main/java/com/example/demo/RestController.java
@@ -1,15 +1,20 @@
 package com.example.demo;
 
+import com.example.demo.TimeConverter.Milliseconds;
 import com.example.demo.domain.Answer;
 import com.example.demo.domain.Question;
+import com.example.demo.domain.SessionInfo;
 import com.example.demo.errors.NoNewQuestionException;
 import com.example.demo.responseClasses.CorrectAnswer;
+import com.example.demo.responseClasses.Results;
+import com.example.demo.responseClasses.SendQuestion;
 import com.example.demo.service.AnswerService;
 import com.example.demo.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -18,77 +23,90 @@ public class RestController {
     QuestionService questionService;
     AnswerService answerService;
     private List<Question> allQuestions;
-    private List<Integer> answeredQuestions;
 
-    public RestController(QuestionService questionService, AnswerService answerService) {
+    @Resource(name = "sessionInfo")
+    private SessionInfo sessionInfo;
+
+    public RestController(QuestionService questionService, AnswerService answerService, SessionInfo sessionInfo) {
         this.questionService = questionService;
         this.answerService = answerService;
+        this.sessionInfo = new SessionInfo();
     }
 
-    public static void main(String[] args) {
-       new RestController();
-    }
-
-    public RestController() {
-        answeredQuestions = new ArrayList<>();
-    }
-
-
-
-
-
-    @GetMapping("/question/{id}")
-    private Question getPerson(@PathVariable("id") int id) {
+    @GetMapping("/questions/{id}")
+    private Question getQuestion(@PathVariable("id") int id) {
         return questionService.getQuestionById(id);
     }
 
-    @DeleteMapping("/question/{id}")
-    private void deletePerson(@PathVariable("id") int id) {
+    @DeleteMapping("/questions/{id}")
+    private void deleteQuestion(@PathVariable("id") int id) {
         questionService.deleteById(id);
     }
 
-//    @PostMapping("/question/save")
-//    private int savePerson(@RequestBody Question question) {
-//        questionService.saveOrUpdate(question);
-//        return question.getId();
-//    }
+    @PostMapping("/questions/question")
+    private int saveQuestion(@RequestBody Question question) {
+        questionService.saveOrUpdate(question);
+        return question.getId();
+    }
 
     @GetMapping("/questions")
     private List<Question> getQuestions() {
         return questionService.getAllQuestions();
     }
 
-    @GetMapping("/question/any")
-    Question getQuestion() throws Exception {
+    @GetMapping("/questions/unanswered/any")
+    SendQuestion getQuestion() throws Exception {
         allQuestions = questionService.getAllQuestions();
-        List<Question> notAnswered = allQuestions.stream().filter(question -> !answeredQuestions.contains(question.getId())).collect(Collectors.toList());
+        List<Question> notAnswered = allQuestions.stream().filter(question -> (!sessionInfo.getAnsweredQuestionsId().contains(question.getId()))).collect(Collectors.toList());
         Random rand = new Random();
         if (notAnswered.isEmpty()) {
+            sessionInfo.setPassedTime();
             throw new NoNewQuestionException("There are no more questions.");
         }
         Question randomQuestion = notAnswered.get(rand.nextInt(notAnswered.size()));
-        return randomQuestion;
+        List<Answer> answersList = answerService.findByQuestionId(randomQuestion.getId());
+        sessionInfo.startTime();
+        return new SendQuestion(randomQuestion, answersList);
     }
-    //@PostMapping("/checkAnswer")
-    //private CorrectAnswer checkAnswer(@RequestBody Integer answerID) {
-
-    //}
-
-    @GetMapping("/answer/{id}")
-    private Answer getAnswersById(@PathVariable("id") int id) {
-        return answerService.getAnswerById(id);
+    @PostMapping("/questions/{id}/answer")
+    CorrectAnswer checkAnswer(@PathVariable("id") Integer questionID, @RequestBody Integer answerID) {
+        List<Answer> answerList = answerService.findByQuestionId(questionID);
+        Boolean isCorrect = answerList.stream().filter(answer -> answer.getId() == answerID).map(Answer::isCorrect).findAny().orElse(null);
+        Question question = questionService.getQuestionById(questionID);
+        sessionInfo.addQuestion(question);
+        String additionalInfo = question.getAdditionalInfo();
+        if (!isCorrect) {
+            Integer rightAnswerID;
+            rightAnswerID = answerList.stream().filter(Answer::isCorrect).map(Answer::getId).findAny().orElse(null);
+            return new CorrectAnswer(question, rightAnswerID, additionalInfo);
+        }
+        sessionInfo.addScore();
+        return new CorrectAnswer(question, additionalInfo);
     }
 
-    @GetMapping("/answers")
+    @GetMapping("/session-info/results")
+    public Results sendResults() {
+        return new Results(sessionInfo.getScore(), sessionInfo.getFinalPassedTime());
+    }
+
+    @GetMapping("/session-info/time/final")
+    public Milliseconds finalPassedTime() {
+        return sessionInfo.getFinalPassedTime();
+    }
+
+    @GetMapping("/session-info/time/current")
+    public Milliseconds currentPassedTime() {
+        return sessionInfo.getCurrentPassedTime();
+    }
+
+    @GetMapping("/session-info/score")
+    Integer score() {
+        return sessionInfo.getScore();
+    }
+
+    @GetMapping("/questions/answers")
     private List<Answer> getAnswers() {
         return answerService.getAllAnswers();
     }
-
-    @GetMapping("/question/{id}/answers")
-    private List<Answer> getAnswersByQuestion (@PathVariable("id") int id){
-        return answerService.findByQuestionId(id);
-    }
-
-
 
 }

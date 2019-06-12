19927ae7c8513a8fe3494cd7762bc0733f3ca270
kristjanm <kristjan.maeots@proekspert.ee>
2019-06-12 13:36:39 +0300

    Merge remote-tracking branch 'remotes/origin/JPG-404' into mkristjan
    # Conflicts:
    #	src/main/resources/data.sql

--- src/main/java/com/example/demo/RestController.java
+++ src/main/java/com/example/demo/RestController.java
@@ -64,10 +64,10 @@ public class RestController {
         List<Question> notAnswered = allQuestions.stream().filter(question -> !answeredQuestions.contains(question.getId())).collect(Collectors.toList());
         Random rand = new Random();
         Question randomQuestion = notAnswered.get(rand.nextInt(notAnswered.size()));
-        if (notAnswered.isEmpty()) {
-            throw new NoNewQuestionException("There are no more questions.");
-        }
-        return randomQuestion;
+        //if (notAnswered.isEmpty()) {
+        throw new NoNewQuestionException("There are no more questions.");
+        //}
+        // return randomQuestion;
     }
     //@PostMapping("/checkAnswer")
     //private CorrectAnswer checkAnswer(@RequestBody Integer answerID) {
